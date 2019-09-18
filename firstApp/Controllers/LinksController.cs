adb00a73655c3f17d3e93f4d1240490a03c93226
YHKero <yassine.khalifii@gmail.com>
2019-09-18 11:05:57 +0200

    first commit on github

---
+++ firstApp/Controllers/LinksController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace firstApp.Controllers
+{
+    public class LinksController : Controller
+    {
+        // GET: Links
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your Links page.";
+            return View();
+        }
+    }
+}
